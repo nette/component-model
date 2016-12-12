c66005e97be1d377b7c2a2045c202ace36c800d4
David Grudl <david@grudl.com>
2016-12-12 12:54:34 +0100

    @return self -> static

--- src/ComponentModel/Component.php
+++ src/ComponentModel/Component.php
@@ -172,7 +172,7 @@ abstract class Component extends Nette\Object implements IComponent
 	 * not be called by applications
 	 * @param  IContainer  New parent or null if this component is being removed from a parent
 	 * @param  string
-	 * @return self
+	 * @return static
 	 * @throws Nette\InvalidStateException
 	 * @internal
 	 */
