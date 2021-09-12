780b48258aaab4c29e9397be226ae23b6dd61af4
David Grudl <david@grudl.com>
2021-09-12 23:36:26 +0200

    used generics in phpDoc [WIP]

--- src/ComponentModel/Component.php
+++ src/ComponentModel/Component.php
@@ -16,8 +16,10 @@ use function func_num_args, in_array, substr;
 /**
  * Base class for all components. Components have a parent, name, and can be monitored by ancestors.
  *
+ * @template T of IContainer
+ * @implements IComponent<T>
  * @property-read string $name
- * @property-read IContainer|null $parent
+ * @property-read T|null $parent
  */
 abstract class Component implements IComponent
 {
@@ -151,6 +153,7 @@ abstract class Component implements IComponent
 
 	/**
 	 * Returns the parent container if any.
+	 * @return T
 	 */
 	final public function getParent(): ?IContainer
 	{
@@ -161,6 +164,7 @@ abstract class Component implements IComponent
 	/**
 	 * Sets or removes the parent of this component. This method is managed by containers and should
 	 * not be called by applications
+	 * @param  T  $parent
 	 * @throws Nette\InvalidStateException
 	 * @internal
 	 */
@@ -202,6 +206,7 @@ abstract class Component implements IComponent
 	/**
 	 * Validates the new parent before it's set.
 	 * Descendant classes can override this to implement custom validation logic.
+	 * @param  T  $parent
 	 * @throws Nette\InvalidStateException
 	 */
 	protected function validateParent(IContainer $parent): void
